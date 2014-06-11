a561ac1cb321f7d186560473fe3131b458cc5472
Jérémy Bouny <jbouny@gmail.com>
2014-06-12 01:54:26 +0200

    Create the main scene and begin a first static version LOD

---
+++ LODnode.js
@@ -0,0 +1,92 @@
+function LODnode() {
+}
+
+LODnode.prototype.create = function create( inX, inY, inSize, inLevel ) {
+	this.x = inX;
+	this.y = inY;
+	this.centerX = inX + inSize * 0.5;
+	this.centerY = inY + inSize * 0.5;
+	this.size = inSize;
+	this.level = inLevel;
+	this.height = 0;
+	this.childs = [];
+	
+	if( 0 === inLevel ) {
+		// If we are a leaf, stop the creation process
+	}
+	else {
+		// Else, create childs
+		var childSize = inSize * 0.5;
+		
+		for( var i = 0; i < 2; ++i ) {
+			for( var j = 0; j < 2; ++j ) {
+				var child = new LODnode();
+				
+				child.create( inX + childSize * j, inY + childSize * i, childSize, inLevel - 1 );
+
+				this.childs[j * 2 + i] = child;
+			}			
+		}
+	}
+}
+
+LODnode.prototype.intersectsSphere = function intersectsSphere( inRange, inPosition ) {
+	var distance = Math.sqrt( 
+		(inPosition.x - this.centerX) * (inPosition.x - this.centerX) +
+		(inPosition.z - this.centerY) * (inPosition.z - this.centerY) +
+		(inPosition.y - this.height) * (inPosition.y - this.height) 
+	);
+	//console.log( distance + " " + inRange );
+	return distance < inRange;
+}
+
+LODnode.prototype.frustumIntersect = function frustumIntersect( inFrustum ) {
+	return true ;
+}
+
+LODnode.prototype.select = function select( inRanges, inFrustum, inPosition, outSelection ) {
+
+	if( !this.intersectsSphere( inRanges[ this.level ], inPosition ) ) {
+		// No node or child nodes were selected; return false so that our parent node handles our area 
+		//console.log("!this.intersectsSphere level");
+		return false;
+	}
+	
+	if( !this.frustumIntersect( inFrustum ) ) {
+		/* We are out of frustum, select nothing but return true to mark this node as having been 
+		   correctly handled so that our parent node does not select itself over our area */
+		//console.log("frustumIntersect");
+		return true; 
+	}
+	
+	if( 0 === this.level ) {
+		// We are in our LOD range and we are the last LOD level 
+		//console.log("0 === this.level");
+		outSelection.nodes.push( this );
+		return true;
+	}
+	else {
+		if( !this.intersectsSphere( inRanges[this.level-1], inPosition ) ) {
+			/* We are in range of our LOD level and we are not the last level: if we are also in range 
+			   of a more detailed LOD level, then some of our child nodes will need to be selected 
+			   instead in order to display a more detailed mesh.
+			   We cover the required lodLevel range */
+			outSelection.nodes.push( this );
+		}
+		else {
+			/* We cover the more detailed lodLevel range: some or all of our four child nodes will 
+			   have to be selected instead */
+			for( var key in this.childs ) {
+				var childNode = this.childs[key];
+				if( !childNode.select( inRanges, inFrustum, inPosition, outSelection ) ) {
+					//console.log("childNode.select?");
+					outSelection.nodes.push( childNode );
+				}
+			}
+		}
+
+		return true;
+	}
+
+
+};
---
+++ LODplane.js
@@ -0,0 +1,187 @@
+function LODSelection() {
+	this.nodes = new Array();
+}
+
+var LODTerrain = function( inWidth, inHeight, inLevels, inLevelResolution ) {
+	THREE.Object3D.call(this);
+
+	this.width = (inWidth !== undefined) ? inWidth : 1;
+	this.height = (inHeight !== undefined) ? inHeight : 1;
+	this.levels = (inLevels !== undefined) ? inLevels : 7;
+	this.levelResolution = (inLevelResolution !== undefined) ? inLevelResolution : 10;
+
+	this.root = new LODnode();
+	this.root.create( 0, 0, 50.0, this.levels );
+	this.selection = new LODSelection();
+}
+
+LODTerrain.prototype.geometry = function geometry( inPosition ) {
+	this.root.select( [ 1,2,4,8,16,32,64,128,256,512 ], 0, inPosition, this.selection );
+	
+	var geo = new THREE.Geometry();
+	var nbVertices = 0;
+	for( var node in this.selection.nodes ) {
+		var lodNode = this.selection.nodes[node];
+		//console.log( lodNode );
+		var size = lodNode.size / this.levelResolution ;
+		for( var i = 0; i < this.levelResolution; ++i ) {
+		
+			var y = lodNode.y + size * i;
+			
+			for( var j = 0; j < this.levelResolution; ++j ) {
+			
+				var x = lodNode.x + size * j;
+		
+				geo.vertices.push( new THREE.Vector3( x, y, 0 ) );
+				geo.vertices.push( new THREE.Vector3( x + size, y, 0 ) );
+				geo.vertices.push( new THREE.Vector3( x + size, y + size, 0 ) );
+				geo.vertices.push( new THREE.Vector3( x, y + size, 0 ) );
+				
+				geo.faces.push( new THREE.Face3( nbVertices, nbVertices + 1, nbVertices + 2 ) );
+				geo.faces.push( new THREE.Face3( nbVertices + 2, nbVertices + 3, nbVertices ) );
+						
+				nbVertices += 4;
+				
+			}
+		}
+	}
+	
+	//this.geometry.computeFaceNormals();
+	//this.geometry.computeVertexNormals();
+	
+	return geo;
+};
+
+var WINDOW = {
+	ms_Width: 0,
+	ms_Height: 0,
+	ms_Callbacks: {
+		70: "WINDOW.toggleFullScreen()"		// Toggle fullscreen
+	},
+	
+	initialize: function initialize() {
+		this.updateSize();
+		
+		// Create callbacks from keyboard
+		$(document).keydown(function(inEvent) { WINDOW.callAction(inEvent.keyCode); }) ;
+		$(window).resize(function(inEvent) {
+			WINDOW.updateSize();
+			WINDOW.resizeCallback(WINDOW.ms_Width, WINDOW.ms_Height);
+		});
+	},
+	updateSize: function updateSize() {
+		this.ms_Width = $(window).width();
+		this.ms_Height = $(window).height() - 4;
+	},
+	callAction: function callAction(inId) {
+		if(inId in this.ms_Callbacks) {
+			eval(this.ms_Callbacks[inId]);
+			return false ;
+		}
+	},
+	toggleFullScreen: function toggleFullScreen() {
+		if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+			if(document.documentElement.requestFullscreen)
+				document.documentElement.requestFullscreen();
+			else if(document.documentElement.mozRequestFullScreen)
+				document.documentElement.mozRequestFullScreen();
+			else if(document.documentElement.webkitRequestFullscreen)
+				document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		} 
+		else  {
+			if(document.cancelFullScreen)
+				document.cancelFullScreen();
+			else if(document.mozCancelFullScreen)
+				document.mozCancelFullScreen();
+			else if (document.webkitCancelFullScreen)
+				document.webkitCancelFullScreen();
+		}
+	},	
+	resizeCallback: function resizeCallback(inWidth, inHeight) {}
+};
+
+var DEMO = {
+	ms_Canvas: null,
+	ms_Renderer: null,
+	ms_Camera: null, 
+	ms_Scene: null, 
+	ms_Controls: null,
+	ms_Water: null,
+
+    enable: (function enable() {
+        try {
+            var aCanvas = document.createElement('canvas');
+            return !! window.WebGLRenderingContext && (aCanvas.getContext('webgl') || aCanvas.getContext('experimental-webgl'));
+        }
+        catch(e) {
+            return false;
+        }
+    })(),
+	
+	initialize: function initialize(inIdCanvas) {
+		this.ms_Canvas = $('#'+inIdCanvas);
+		
+		// Initialize Renderer, Camera and Scene
+		this.ms_Renderer = this.enable? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+		this.ms_Canvas.html(this.ms_Renderer.domElement);
+		this.ms_Scene = new THREE.Scene();
+		
+		this.ms_Camera = new THREE.PerspectiveCamera(55.0, WINDOW.ms_Width / WINDOW.ms_Height, 0.5, 3000000);
+		this.ms_Camera.position.set(100, 50, -150);
+		this.ms_Camera.lookAt(new THREE.Vector3(0, 0, 0));
+		
+		// Initialize Orbit control		
+		this.ms_Controls = new THREE.OrbitControls( this.ms_Camera );
+		//this.ms_Controls = new THREE.OrbitControls(this.ms_Camera, this.ms_Renderer.domElement);
+	
+		// Add light
+		var directionalLight = new THREE.DirectionalLight(0xffff55, 1);
+		directionalLight.position.set(-600, 300, 600);
+		this.ms_Scene.add(directionalLight);
+		
+		// Create LOD terrain
+		
+		this.ms_LODTerrain = new LODTerrain();
+		
+		var geometry = new THREE.PlaneGeometry( 1000, 1000, 20, 20 );
+		this.ms_Material = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true, side: THREE.DoubleSide} );
+		this.ms_Plane = new THREE.Mesh( this.ms_LODTerrain.geometry( new THREE.Vector3( 0, 0, 0 ) ), this.ms_Material );
+		this.ms_Scene.add( this.ms_Plane );
+	},
+
+    display: function display() {
+		this.ms_Renderer.render(this.ms_Scene, this.ms_Camera);
+	},
+	
+	update: function update() {
+		//this.ms_Scene.remove( this.ms_Plane );
+		//this.ms_Plane = new THREE.Mesh( this.ms_LODTerrain.geometry( this.ms_Camera.position ), this.ms_Material );
+		//this.ms_Scene.add( this.ms_Plane );
+		
+		this.display();
+	},
+	
+	resize: function resize(inWidth, inHeight) {
+		this.ms_Camera.aspect =  inWidth / inHeight;
+		this.ms_Camera.updateProjectionMatrix();
+		this.ms_Renderer.setSize(inWidth, inHeight);
+		this.ms_Canvas.html(this.ms_Renderer.domElement);
+		this.display();
+	}
+};
+
+function mainLoop() {
+    requestAnimationFrame(mainLoop);
+    DEMO.update();
+}
+
+$(function() {
+	WINDOW.initialize();
+	
+	DEMO.initialize('canvas-3d');
+	
+	WINDOW.resizeCallback = function(inWidth, inHeight) { DEMO.resize(inWidth, inHeight); };
+	DEMO.resize(WINDOW.ms_Width, WINDOW.ms_Height);
+
+    mainLoop();
+});
