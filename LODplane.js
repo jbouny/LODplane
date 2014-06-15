4a999b60952230be112cc97766f09d26149c7021
Jérémy Bouny <jbouny@gmail.com>
2014-06-15 12:02:10 +0200

    Finalize basic implementation of CDLOD
    Color generation and review architecture

--- LODplane.js
+++ LODplane.js
@@ -1,89 +1,205 @@
-function LODSelection() {
-	this.nodes = new Array();
-	this.boxes = new Array();
+var LOD = {};
+
+LOD.Node = function Node() {
 }
 
-LODSelection.prototype.add = function add( inNode, inX, inY, inPart ) {
-	var box = [];
-	box[0] = (inX !== undefined) ? inX : inNode.x;
-	box[1] = (inY !== undefined) ? inY : inNode.y;
-	box[2] = (inPart !== undefined) ? inPart : false;
+LOD.Node.prototype.create = function create( inX, inY, inSize, inLevel ) {
+	this.x = inX;
+	this.y = inY;
+	this.centerX = inX + inSize * 0.5;
+	this.centerY = inY + inSize * 0.5;
+	this.size = inSize;
+	this.level = inLevel;
+	this.height = 0;
+	this.childs = [];
 	
-	this.nodes.push( inNode );
-	this.boxes.push( box );
+	var nbNodes = 0;
+	
+	if( 0 === inLevel ) {
+		// If we are a leaf, stop the creation process
+		nbNodes = 1;
+	}
+	else {
+		// Else, create childs
+		var childSize = inSize * 0.5;
+		
+		for( var i = 0; i < 2; ++i ) {
+			for( var j = 0; j < 2; ++j ) {
+				var child = new LOD.Node();
+				
+				nbNodes += child.create( inX + childSize * j, inY + childSize * i, childSize, inLevel - 1 );
+
+				this.childs[j * 2 + i] = child;
+			}			
+		}
+	}
+	
+	return nbNodes;
 }
 
-var LODTerrain = function( inSize, inLevels, inLevelResolution ) {
+LOD.Node.prototype.intersectsSphere = function intersectsSphere( inRange, inPosition ) {
+	var xp = this.centerX;
+	var yp = this.height;
+	var zp = this.centerY;
+	
+	var distance = Math.sqrt( 
+		(inPosition.x - xp) * (inPosition.x - xp) +
+		(inPosition.z - yp) * (inPosition.z - yp) +
+		(inPosition.y - zp) * (inPosition.y - zp) 
+	);
+	
+	return ( distance - this.size * 0.5 ) < inRange;
+}
+
+LOD.Node.prototype.frustumIntersect = function frustumIntersect( inFrustum ) {
+	return true ;
+}
+
+LOD.Node.prototype.select = function select( inRanges, inFrustum, inPosition, outSelection ) {
+
+	if( !this.intersectsSphere( inRanges[ this.level ], inPosition ) ) {
+		if( outSelection.nbLevels === this.level ) {
+			// If we are the root, handle even if we are away
+			outSelection.add( this.x, this.y, this.size, this.level, 0 );
+			return true;
+		}
+		else {
+			// No node or child nodes were selected; return false so that our parent node handles our area
+			return false;
+		}
+	}
+	
+	if( !this.frustumIntersect( inFrustum ) ) {
+		/* We are out of frustum, select nothing but return true to mark this node as having been 
+		   correctly handled so that our parent node does not select itself over our area */
+		//console.log("frustumIntersect");
+		return true; 
+	}
+	
+	if( 0 === this.level ) {
+		// We are in our LOD range and we are the last LOD level 
+		outSelection.add( this.x, this.y, this.size, this.level, 0 );
+	}
+	else {
+		/* We are in range of our LOD level and we are not the last level: if we are also in range 
+		   of a more detailed LOD level, then some of our child nodes will need to be selected 
+		   instead in order to display a more detailed mesh. */
+		if( !this.intersectsSphere( inRanges[this.level-1], inPosition ) ) {
+			// We cover the required lodLevel range
+			outSelection.add( this.x, this.y, this.size, this.level, 0 );
+		}
+		else {
+			/* We cover the more detailed lodLevel range: some or all of our four child nodes will 
+			   have to be selected instead */
+			for( var key in this.childs ) {
+				var childNode = this.childs[key];
+				
+				if( !childNode.select( inRanges, inFrustum, inPosition, outSelection ) ) {
+					outSelection.add( childNode.x, childNode.y, this.size, this.level, 1 );
+				}
+			}
+		}
+	}
+	return true;
+};
+
+LOD.Selection = function Selection( inNbNodes, inNbLevels ) {
+	this.posX = new Float32Array( inNbNodes  );
+	this.posY = new Float32Array( inNbNodes );
+	this.size = new Float32Array( inNbNodes );
+	this.partition = new Int8Array( inNbNodes ) ;
+	this.level = new Int8Array( inNbNodes ) ;
+	this.nbElements = 0;
+	this.nbLevels = inNbLevels;
+}
+
+LOD.Selection.prototype.clear = function clear() {
+	this.nbElements = 0;
+}
+
+LOD.Selection.prototype.add = function add( inX, inY, inSize, inLevel, inPartition ) {
+	var area = [];
+	this.posX[this.nbElements] = inX;
+	this.posY[this.nbElements] = inY;
+	this.size[this.nbElements] = inSize;
+	this.partition[this.nbElements] = inPartition;
+	this.level[this.nbElements] = inLevel;
+	
+	this.nbElements ++;
+}
+
+LOD.Plane = function Plane( inSize, inLevels, inLevelResolution ) {
 	THREE.Object3D.call(this);
 
 	this.size = (inSize !== undefined) ? inSize : 1;
 	this.levels = (inLevels !== undefined) ? inLevels : 7;
 	this.levelResolution = (inLevelResolution !== undefined) ? inLevelResolution : 10;
 
-	this.root = new LODnode();
-	this.root.create( - this.size * 0.5, - this.size * 0.5, this.size, this.levels );
-	this.selection = new LODSelection();
+	this.root = new LOD.Node();
+	var nbNodes = this.root.create( - this.size * 0.5, - this.size * 0.5, this.size, this.levels - 1 );
+	this.selection = new LOD.Selection( nbNodes, inLevels - 1 );
+	
+	// Generate ranges
+	this.ranges = [];
+	var range = 1;
+	for( var i = 0; i < inLevels; ++i ) {
+		this.ranges[i] = range;
+		range *= 2;
+	}
+	
+	// Generate tiles colors
+	this.tilesColors = [];
+	this.tilesColorsDark = [];
+	var startColor = new THREE.Color( 0xff00ff );
+	var endColor = new THREE.Color( 0x0000ff );
+	for( var i = 0; i < this.levels; ++i ) {
+		var distance = i / ( this.levels - 1 );
+		var color = new THREE.Color(
+			startColor.r + ( endColor.r - startColor.r ) * distance,
+			startColor.g + ( endColor.g - startColor.g ) * distance,
+			startColor.b + ( endColor.b - startColor.b ) * distance
+		);
+		this.tilesColors[i] = color;
+		this.tilesColorsDark[i] = new THREE.Color( color.r * 0.3, color.g * 0.3, color.b * 0.3 );
+	}
 }
 
-LODTerrain.prototype.geometry = function geometry( inPosition ) {
-	this.root.select( [ 1,2,4,8,16,32,64,128,256,512 ], 0, inPosition, this.selection );
+LOD.Plane.prototype.geometry = function geometry( inPosition ) {
+	this.root.select( this.ranges, 0, inPosition, this.selection );
 	
-	// THREE.Geometry version
-	/*var geo = new THREE.Geometry();
-	var nbVertices = 0;
-	for( var node in this.selection.nodes ) {
-		var lodNode = this.selection.nodes[node];
-		//console.log( lodNode );
-		var size = lodNode.size / this.levelResolution ;
-		for( var i = 0; i < this.levelResolution; ++i ) {
-		
-			var y = lodNode.y + size * i;
-			
-			for( var j = 0; j < this.levelResolution; ++j ) {
-			
-				var x = lodNode.x + size * j;
-		
-				geo.vertices.push( new THREE.Vector3( x, y, 0 ) );
-				geo.vertices.push( new THREE.Vector3( x + size, y, 0 ) );
-				geo.vertices.push( new THREE.Vector3( x + size, y + size, 0 ) );
-				geo.vertices.push( new THREE.Vector3( x, y + size, 0 ) );
-				
-				geo.faces.push( new THREE.Face3( nbVertices, nbVertices + 1, nbVertices + 2 ) );
-				geo.faces.push( new THREE.Face3( nbVertices + 2, nbVertices + 3, nbVertices ) );
-						
-				nbVertices += 4;
-				
-			}
-		}
-	}*/
 	
-	// THREE.BufferGeometry version
 	var geo = new THREE.BufferGeometry();
 	
-	var triangles = this.selection.nodes.length * 2 * this.levelResolution * this.levelResolution ;
+	var nbNodes = this.selection.nbElements;
+	var triangles = nbNodes * 2 * this.levelResolution * this.levelResolution ;
 	geo.addAttribute( 'index', new THREE.Uint16Attribute( triangles * 3, 1 ) );
+	geo.addAttribute( 'color', new THREE.Float32Attribute( triangles * 3 * 3, 3 ) );
 	geo.addAttribute( 'position', new THREE.Float32Attribute( triangles * 3 * 3, 3 ) );
 	
+	var colors = geo.getAttribute( 'color' ).array;
 	var positions = geo.getAttribute( 'position' ).array;
 	var indices = geo.getAttribute( 'index' ).array;
 	var indexPosition = 0;
 	var indexIndices = 0;
 	var nbVertices = 0;
 	
-	for( var node in this.selection.nodes ) {
-		var lodNode = this.selection.nodes[node];
-		var lodBox = this.selection.boxes[node];
-		var resolution = lodBox[2] ? this.levelResolution  / 2 : this.levelResolution;
-		var size = lodNode.size / resolution ;
+	for( var node = 0; node < nbNodes; node++ ) {
+		var nodePartition = this.selection.partition[node];
+		var nodeSize = this.selection.size[node];
+		var nodeX = this.selection.posX[node];
+		var nodeY = this.selection.posY[node];
+		var nodeLevel = this.selection.level[node];
+		var nbTiles = ( 1 === nodePartition ) ? ( this.levelResolution  / 2 ) : this.levelResolution; // Check if this is just a demi area
+		var size = nodeSize / this.levelResolution ;
 		
-		for( var i = 0; i < resolution; ++i ) {
+		for( var i = 0; i < nbTiles; ++i ) {
 		
-			var y = lodBox[1] + size * i;
+			var y = nodeY + size * i;
 			
-			for( var j = 0; j < resolution; ++j ) {
+			for( var j = 0; j < nbTiles; ++j ) {
 				var p = indexPosition;
 				var id = indexIndices;
-				var x = lodBox[0] + size * j;
+				var x = nodeX + size * j;
 				
 				positions[p] = x;
 				positions[p+1] = y;
@@ -105,9 +221,16 @@ LODTerrain.prototype.geometry = function geometry( inPosition ) {
 				indices[id+1] = nbVertices + 1;
 				indices[id+2] = nbVertices + 2;
 				
-				//indices[id+3] = nbVertices + 2;
-				//indices[id+4] = nbVertices + 3;
-				//indices[id+5] = nbVertices;
+				indices[id+3] = nbVertices + 2;
+				indices[id+4] = nbVertices + 3;
+				indices[id+5] = nbVertices;
+				
+				var color = ( ( i % 2 + j % 2 ) == 1 ) ? this.tilesColors[nodeLevel] : this.tilesColorsDark[nodeLevel] ;
+				for( var pos = 0; pos < 4; pos++ ) {
+					colors[p + pos * 3] = color.r;
+					colors[p + pos * 3 + 1] = color.g;
+					colors[p + pos * 3 + 2] = color.b;
+				}
 						
 				nbVertices += 4;
 				indexIndices += 6;
@@ -117,146 +240,10 @@ LODTerrain.prototype.geometry = function geometry( inPosition ) {
 		}
 	}
 	
-	
-	this.selection.nodes = [];
+	this.selection.clear();
 	
 	//this.geometry.computeFaceNormals();
 	//this.geometry.computeVertexNormals();
 	
 	return geo;
 };
-
-var WINDOW = {
-	ms_Width: 0,
-	ms_Height: 0,
-	ms_Callbacks: {
-		70: "WINDOW.toggleFullScreen()"		// Toggle fullscreen
-	},
-	
-	initialize: function initialize() {
-		this.updateSize();
-		
-		// Create callbacks from keyboard
-		$(document).keydown(function(inEvent) { WINDOW.callAction(inEvent.keyCode); }) ;
-		$(window).resize(function(inEvent) {
-			WINDOW.updateSize();
-			WINDOW.resizeCallback(WINDOW.ms_Width, WINDOW.ms_Height);
-		});
-	},
-	updateSize: function updateSize() {
-		this.ms_Width = $(window).width();
-		this.ms_Height = $(window).height() - 4;
-	},
-	callAction: function callAction(inId) {
-		if(inId in this.ms_Callbacks) {
-			eval(this.ms_Callbacks[inId]);
-			return false ;
-		}
-	},
-	toggleFullScreen: function toggleFullScreen() {
-		if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
-			if(document.documentElement.requestFullscreen)
-				document.documentElement.requestFullscreen();
-			else if(document.documentElement.mozRequestFullScreen)
-				document.documentElement.mozRequestFullScreen();
-			else if(document.documentElement.webkitRequestFullscreen)
-				document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-		} 
-		else  {
-			if(document.cancelFullScreen)
-				document.cancelFullScreen();
-			else if(document.mozCancelFullScreen)
-				document.mozCancelFullScreen();
-			else if (document.webkitCancelFullScreen)
-				document.webkitCancelFullScreen();
-		}
-	},	
-	resizeCallback: function resizeCallback(inWidth, inHeight) {}
-};
-
-var DEMO = {
-	ms_Canvas: null,
-	ms_Renderer: null,
-	ms_Camera: null, 
-	ms_Scene: null, 
-	ms_Controls: null,
-	ms_Water: null,
-
-    enable: (function enable() {
-        try {
-            var aCanvas = document.createElement('canvas');
-            return !! window.WebGLRenderingContext && (aCanvas.getContext('webgl') || aCanvas.getContext('experimental-webgl'));
-        }
-        catch(e) {
-            return false;
-        }
-    })(),
-	
-	initialize: function initialize(inIdCanvas) {
-		this.ms_Canvas = $('#'+inIdCanvas);
-		
-		// Initialize Renderer, Camera and Scene
-		this.ms_Renderer = this.enable? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
-		this.ms_Canvas.html(this.ms_Renderer.domElement);
-		this.ms_Scene = new THREE.Scene();
-		
-		this.ms_Camera = new THREE.PerspectiveCamera(55.0, WINDOW.ms_Width / WINDOW.ms_Height, 0.5, 3000000);
-		this.ms_Camera.position.set(100, 50, -150);
-		this.ms_Camera.lookAt(new THREE.Vector3(0, 0, 0));
-		
-		// Initialize Orbit control		
-		this.ms_Controls = new THREE.OrbitControls( this.ms_Camera );
-		this.ms_Controls.addEventListener( 'change', this.lodUpdate );
-		//this.ms_Controls = new THREE.OrbitControls(this.ms_Camera, this.ms_Renderer.domElement);
-	
-		// Add light
-		var directionalLight = new THREE.DirectionalLight(0xffff55, 1);
-		directionalLight.position.set(-600, 300, 600);
-		this.ms_Scene.add(directionalLight);
-		
-		// Create LOD terrain
-		this.ms_LODTerrain = new LODTerrain( 200, 7, 10 );
-		
-		this.ms_Material = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true, side: THREE.DoubleSide} );
-		this.ms_Plane = new THREE.Mesh( this.ms_LODTerrain.geometry( new THREE.Vector3( 0, 0, 0 ) ), this.ms_Material );
-		this.ms_Scene.add( this.ms_Plane );
-	},
-
-    display: function display() {
-		this.ms_Renderer.render(this.ms_Scene, this.ms_Camera);
-	},
-	
-	lodUpdate: function lodUpdate() {
-		DEMO.ms_Scene.remove( DEMO.ms_Plane );
-		DEMO.ms_Plane = new THREE.Mesh( DEMO.ms_LODTerrain.geometry( DEMO.ms_Camera.position ), DEMO.ms_Material );
-		DEMO.ms_Scene.add( DEMO.ms_Plane );
-	},
-	
-	update: function update() {
-		this.display();
-	},
-	
-	resize: function resize(inWidth, inHeight) {
-		this.ms_Camera.aspect =  inWidth / inHeight;
-		this.ms_Camera.updateProjectionMatrix();
-		this.ms_Renderer.setSize(inWidth, inHeight);
-		this.ms_Canvas.html(this.ms_Renderer.domElement);
-		this.display();
-	}
-};
-
-function mainLoop() {
-    requestAnimationFrame(mainLoop);
-    DEMO.update();
-}
-
-$(function() {
-	WINDOW.initialize();
-	
-	DEMO.initialize('canvas-3d');
-	
-	WINDOW.resizeCallback = function(inWidth, inHeight) { DEMO.resize(inWidth, inHeight); };
-	DEMO.resize(WINDOW.ms_Width, WINDOW.ms_Height);
-
-    mainLoop();
-});
---
+++ demo.js
@@ -0,0 +1,126 @@
+var WINDOW = {
+	ms_Width: 0,
+	ms_Height: 0,
+	ms_Callbacks: {
+		70: "WINDOW.toggleFullScreen()"		// Toggle fullscreen
+	},
+	
+	initialize: function initialize() {
+		this.updateSize();
+		
+		// Create callbacks from keyboard
+		$(document).keydown(function(inEvent) { WINDOW.callAction(inEvent.keyCode); }) ;
+		$(window).resize(function(inEvent) {
+			WINDOW.updateSize();
+			WINDOW.resizeCallback(WINDOW.ms_Width, WINDOW.ms_Height);
+		});
+	},
+	updateSize: function updateSize() {
+		this.ms_Width = $(window).width();
+		this.ms_Height = $(window).height() - 4;
+	},
+	callAction: function callAction(inId) {
+		if(inId in this.ms_Callbacks) {
+			eval(this.ms_Callbacks[inId]);
+			return false ;
+		}
+	},
+	toggleFullScreen: function toggleFullScreen() {
+		if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+			if(document.documentElement.requestFullscreen)
+				document.documentElement.requestFullscreen();
+			else if(document.documentElement.mozRequestFullScreen)
+				document.documentElement.mozRequestFullScreen();
+			else if(document.documentElement.webkitRequestFullscreen)
+				document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		} 
+		else  {
+			if(document.cancelFullScreen)
+				document.cancelFullScreen();
+			else if(document.mozCancelFullScreen)
+				document.mozCancelFullScreen();
+			else if (document.webkitCancelFullScreen)
+				document.webkitCancelFullScreen();
+		}
+	},	
+	resizeCallback: function resizeCallback(inWidth, inHeight) {}
+};
+
+var DEMO = {
+	ms_Canvas: null,
+	ms_Renderer: null,
+	ms_Camera: null, 
+	ms_Scene: null, 
+	ms_Controls: null,
+	ms_Water: null,
+
+    enable: (function enable() {
+        try {
+            var aCanvas = document.createElement('canvas');
+            return !! window.WebGLRenderingContext && (aCanvas.getContext('webgl') || aCanvas.getContext('experimental-webgl'));
+        }
+        catch(e) {
+            return false;
+        }
+    })(),
+	
+	initialize: function initialize( inIdCanvas ) {
+		this.ms_Canvas = $( '#' + inIdCanvas );
+		
+		// Initialize Renderer, Camera and Scene
+		this.ms_Renderer = this.enable? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+		this.ms_Canvas.html( this.ms_Renderer.domElement );
+		this.ms_Scene = new THREE.Scene();
+		
+		this.ms_Camera = new THREE.PerspectiveCamera( 55.0, WINDOW.ms_Width / WINDOW.ms_Height, 0.001, 3000000 );
+		this.ms_Camera.position.set( 0, 0, -200 );
+		this.ms_Camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+		
+		// Initialize Orbit control		
+		this.ms_Controls = new THREE.OrbitControls( this.ms_Camera );
+		this.ms_Controls.addEventListener( 'change', this.lodUpdate );
+		
+		// Create LOD terrain
+		this.ms_LODTerrain = new LOD.Plane( 200, 7, 16 );
+		this.ms_Material = new THREE.MeshBasicMaterial( {vertexColors: THREE.VertexColors, wireframe: true, side: THREE.DoubleSide} );
+		this.lodUpdate();
+	},
+
+    display: function display() {
+		this.ms_Renderer.render( this.ms_Scene, this.ms_Camera );
+	},
+	
+	lodUpdate: function lodUpdate() {
+		DEMO.ms_Scene.remove( DEMO.ms_Plane );
+		DEMO.ms_Plane = new THREE.Mesh( DEMO.ms_LODTerrain.geometry( DEMO.ms_Camera.position ), DEMO.ms_Material );
+		DEMO.ms_Scene.add( DEMO.ms_Plane );
+	},
+	
+	update: function update() {
+		this.display();
+	},
+	
+	resize: function resize( inWidth, inHeight ) {
+		this.ms_Camera.aspect =  inWidth / inHeight;
+		this.ms_Camera.updateProjectionMatrix();
+		this.ms_Renderer.setSize( inWidth, inHeight );
+		this.ms_Canvas.html( this.ms_Renderer.domElement );
+		this.display();
+	}
+};
+
+function mainLoop() {
+    requestAnimationFrame(mainLoop);
+    DEMO.update();
+}
+
+$(function() {
+	WINDOW.initialize();
+	
+	DEMO.initialize('canvas-3d');
+	
+	WINDOW.resizeCallback = function(inWidth, inHeight) { DEMO.resize(inWidth, inHeight); };
+	DEMO.resize(WINDOW.ms_Width, WINDOW.ms_Height);
+
+    mainLoop();
+});
